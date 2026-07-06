56cc068635f7d717a2eb942ec8ba4b30f8e633a2
GGS <ggs007@yandex.ru>
2026-07-06 16:52:31 +0500

    2026/07/06 Большая редакция

--- pre-early-init.el
+++ pre-early-init.el
@@ -1,5 +1,10 @@
 ;;; pre-early-init.el --- Pre-early init file -*- no-byte-compile: t; lexical-binding: t; -*-
-;(setq minimal-emacs-ui-features '(context-menu tool-bar menu-bar dialogs tooltips))
+;;(setq minimal-emacs-ui-features '(context-menu tool-bar menu-bar dialogs tooltips))
+;;; Уменьшение беспорядка в ~/.emacs.d путём перенаправления файлов в ~/.emacs.d/var/
+;; ПРИМЕЧАНИЕ: Это должно быть помещено в 'pre-early-init.el'.
+(setq user-emacs-directory (expand-file-name "var/" minimal-emacs-user-directory))
+(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+
 (defun display-startup-time ()
   "Display the startup time and number of garbage collections." ;
   (message "Emacs init loaded in %.2f seconds (Full emacs-startup: %.2fs) with %d garbage collections."
